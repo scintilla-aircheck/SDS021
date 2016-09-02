c9a9a0960a418260cf6c977a21e510ed76e66792
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-09-02 11:35:08 -0700

    Copy SDS021 *.h and *.cpp files from Adjutant repository dev branch

---
+++ SDS021.cpp
@@ -0,0 +1,187 @@
+#include "SDS021.h"
+
+
+SDS021::SDS021(byte rx_pin, byte tx_pin) : SoftwareSerial_(rx_pin, tx_pin)
+{
+	ID_ = 0xFFFF;
+	PM2_5_ = 0.0;
+	PM10_ = 0.0;
+	Mode_ = false;
+	State_ = true;
+	Interval_ = 0;
+	Firmware_ = { 15, 7, 10 };
+}
+
+
+SDS021::~SDS021() {}
+
+void SDS021::Begin()
+{
+	SoftwareSerial_.begin(kBaudRate_);
+}
+
+int SDS021::ID()
+{
+	return ID_;
+}
+
+void SDS021::ID(int id)
+{
+	byte* buffer = MakeMessage(EAction::Id, true, ID_);
+	buffer[13] = id >> 8;
+	buffer[14] = id & 0xFF;
+	WriteMessage(buffer);
+}
+
+bool SDS021::PassiveMode()
+{
+	return Mode_;
+}
+
+void SDS021::PassiveMode(bool passive)
+{
+	byte* buffer = MakeMessage(EAction::Mode, true, ID_);
+	buffer[4] = passive;
+	WriteMessage(buffer);
+}
+
+bool SDS021::Awake()
+{
+	return State_;
+}
+
+void SDS021::Awake(bool working)
+{
+	byte* buffer = MakeMessage(EAction::State, true, ID_);
+	buffer[4] = working;
+	WriteMessage(buffer);
+}
+
+byte SDS021::Interval()
+{
+	return Interval_;
+}
+
+void SDS021::Interval(byte minutes)
+{
+	// Clamps minutes between 0-30
+	if (minutes > 30)
+		minutes = 30;
+	byte* buffer = MakeMessage(EAction::Interval, true, ID_);
+	buffer[4] = minutes;
+	WriteMessage(buffer);
+}
+
+float SDS021::PM2_5()
+{
+	return PM2_5_;
+}
+
+float SDS021::PM10()
+{
+	return PM10_;
+}
+
+void SDS021::Query()
+{
+	byte* buffer = MakeMessage(EAction::Query, false, ID_);
+	WriteMessage(buffer);
+}
+
+bool SDS021::Update()
+{
+	bool updated = false;
+	byte buffer[kInputLength_];
+
+	// Check serial buffer for a message
+	while (SoftwareSerial_.available() >= 10)
+	{
+		// Clear the buffer
+		for (int i = 0; i < kInputLength_; i++)
+			buffer[i] = 0;
+
+		// Read the next message
+		SoftwareSerial_.readBytesUntil((byte)EMessage::Tail, buffer, kInputLength_);
+
+		if (CheckSum(buffer))
+		{
+			ECommandId command_id = (ECommandId)buffer[1];
+
+			// Parse data frame
+			if (command_id == ECommandId::Data)
+			{
+				PM2_5_ = (float)CrunchBytes(buffer[3], buffer[2]) / 10;
+				PM10_ = (float)CrunchBytes(buffer[5], buffer[4]) / 10;
+			}
+
+			// Parse reply frame
+			else if (command_id == ECommandId::Reply)
+			{
+				EAction action = (EAction)buffer[2];
+				
+				if (action == EAction::Mode)
+					Mode_ = buffer[4];
+				else if (action == EAction::State)
+					State_ = buffer[4];
+				else if (action == EAction::Interval)
+					Interval_ = buffer[4];
+				else if (action == EAction::Version)
+					Firmware_ = { buffer[3], buffer[4], buffer[5] };
+			}
+
+			// Update device ID
+			ID_ = CrunchBytes(buffer[6], buffer[7]);
+		}
+	}
+
+	return updated;
+}
+
+byte* SDS021::MakeMessage(EAction action, bool set, int address)
+{
+	// Creates a new byte array of zeroes
+	byte* buffer = new byte[kOutputLength_] { 0 };
+
+	// Sets common message values
+	buffer[0] = (byte)EMessage::Head;
+	buffer[1] = (byte)ECommandId::Command;
+	buffer[2] = (byte)action;
+	buffer[3] = (byte)set;
+	buffer[15] = address >> 8;
+	buffer[16] = address & 0xFF;
+	buffer[kOutputLength_ - 1] = (byte)EMessage::Tail;
+
+	return buffer;
+}
+
+void SDS021::WriteMessage(byte* buffer)
+{
+	// Calculates and stores checksum in output buffer
+	buffer[17] = calcCheckSum(buffer, 2, 17);
+
+	SoftwareSerial_.write(buffer, kOutputLength_);
+
+	// Waits for software serial to finish sending message
+	delay(16); // (19 bytes * 8 bits * 0.104 ms = 15.808 ms)
+
+	delete[] buffer;
+}
+
+byte SDS021::calcCheckSum(byte* buffer, int start_idx, int stop_idx)
+{
+	int chk = 0;
+	for (int i = start_idx; i < stop_idx; i++)
+		chk += buffer[i];
+	chk &= 0xFF;
+	return chk;
+}
+
+bool SDS021::CheckSum(byte* buffer)
+{
+		return calcCheckSum(buffer, 2, 8) == buffer[8];
+}
+
+int SDS021::CrunchBytes(byte high_byte, byte low_byte)
+{
+	return ((int)high_byte << 8) + (int)low_byte;
+}
---
+++ SDS021.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+
+class SDS021
+{
+public:
+	enum class EMessage
+	{
+		Head = 0xAA,
+		Tail = 0xAB
+	};
+
+	enum class ECommandId
+	{
+		Command = 0xB4,
+		Data = 0xC0,
+		Reply = 0xC5
+	};
+
+	enum class EAction
+	{
+		Mode = 0x2,
+		Query = 0x4,
+		Id = 0x5,
+		State = 0x6,
+		Version = 0x7,
+		Interval = 0x8,
+	};
+
+	struct Version
+	{
+		byte Year;
+		byte Month;
+		byte Day;
+	};
+
+	/// Instantiates a new SDS021 controller with an associated software serial connection
+	SDS021(byte rx_pin, byte tx_pin);
+	~SDS021();
+
+	/// Initializes the software serial connection
+	void Begin();
+
+	/// Gets/sets the current sensor ID association
+	int ID();
+	void ID(int id = 0xFFFF);
+
+	/// Gets/sets a sensor's mode (true is passive/query mode)
+	bool PassiveMode();
+	void PassiveMode(bool passive);
+
+	/// Gets/sets a sensor's state (i.e. sleep/working)
+	bool Awake();
+	void Awake(bool awake);
+
+	/// Gets/sets a sensor's active reporting interval
+	byte Interval();
+	void Interval(byte minutes);
+
+	/// Gets the most recent PM 2.5 dust reading
+	float PM2_5();
+
+	/// Gets the most recent PM 10 dust reading
+	float PM10();
+
+	/// Queries the device for an updated data frame
+	void Query();
+
+	/// Checks serial buffer for updated data
+	bool Update();
+
+private:
+	// Software serial connection
+	SoftwareSerial SoftwareSerial_;
+	
+	// Message properties
+	static const byte kBaudRate_ = 9600;
+	static const byte kOutputLength_ = 19;
+	static const byte kInputLength_ = 10;
+
+	// SDS021 data cache
+	int ID_;
+	float PM2_5_;
+	float PM10_;
+	bool Mode_;
+	bool State_;
+	byte Interval_;
+	Version Firmware_;
+
+	/// Creates a new message buffer array with common fields set
+	byte* MakeMessage(EAction action, bool set = 0, int address = 0xFFFF);
+
+	/// Writes and destroys an existing message buffer array
+	void WriteMessage(byte* buffer);
+
+	/// Calculates a checksum byte for a given range of bytes in an array
+	byte calcCheckSum(byte* buffer, int start_idx, int stop_idx);
+
+	/// Validates a messages checksum byte
+	bool CheckSum(byte* buffer);
+
+	/// Combines a high and low byte into an integer
+	int CrunchBytes(byte high_byte, byte low_byte);
+};
